9c89964d62e4c8ee041091e4a6dcbca8a57059fc
Reshma-Nayak123 <106798480+Reshma-Nayak123@users.noreply.github.com>
2022-09-10 16:43:53 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/INSTRUQT1/network/google"
-  version = "3.4.1"
+  version = "3.4.0"
   # insert required variables here
 
   network_name= "eshma-network"
